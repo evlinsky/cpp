07cab2591574e25a1c9026ae3ea531d7073640b2
Artur Huletski <hatless.fox@gmail.com>
2016-11-11 12:14:52 +0300

    [Lab 08] Init commit

---
+++ lab_08/include/matrix.h
@@ -0,0 +1,33 @@
+#ifndef _MATRIX_H_INCLUDED
+#define _MATRIX_H_INCLUDED
+
+#include <cstdio>
+#include <cstddef>
+
+class Matrix {
+public:
+  Matrix(std::size_t r, std::size_t c);
+
+  std::size_t get_rows();
+  std::size_t get_cols();
+  void set(std::size_t i, std::size_t j, int val);
+  int get(std::size_t i, std::size_t j);
+  void print(FILE *f);
+
+  Matrix operator+(Matrix& m);
+  Matrix operator-(Matrix& m);
+  Matrix operator*(Matrix& m);
+
+  Matrix& operator+=(Matrix& m);
+  Matrix& operator-=(Matrix& m);
+  Matrix& operator*=(Matrix& m);
+
+  bool operator==(Matrix& m);
+  bool operator!=(Matrix& m);
+private:
+  std::size_t _rows;
+  std::size_t _cols;
+  int **_data;
+};
+
+#endif
