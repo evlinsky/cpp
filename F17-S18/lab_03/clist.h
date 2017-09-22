34fb1d8218f761e745a56a10c63c4a8b8dc8c6cf
Hatless Fox <hatless.fox@gmail.com>
2017-09-22 14:15:13 +0300

    [F17][Lab03] init commit lab3

---
+++ F17-S18/lab_03/clist.h
@@ -0,0 +1,14 @@
+struct intrusive_node {
+  struct intrusive_node* next;
+  struct intrusive_node* prev;
+};
+
+struct intrusive_list {
+  struct intrusive_node* head;
+};
+
+/* ? */ init_list(intrusive_list /* ? */);
+/* ? */ add_node(intrusive_list /* ? */, intrusive_node /* ? */);
+/* ? */ remove_node(intrusive_list /* ? */, intrusive_node /* ? */);
+
+int get_length(intrusive_list /* ? */ );
