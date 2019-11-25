e8b832d1b8c036a7c987acc435058f6d60198ae0
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2019-11-25 11:49:29 +0300

    SPbSU: lab_09: initial push.

---
+++ SPbU_F19-S20/lab_09/include/shared_ptr.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "matrix.hpp"
+
+class shared_ptr {
+public:
+    shared_ptr(Matrix* obj = 0);
+    shared_ptr(const shared_ptr& other);
+    shared_ptr& operator=(shared_ptr other);
+    ~shared_ptr();
+
+    Matrix* ptr() const;
+    bool isNull() const;
+    void reset(Matrix* obj = 0);
+
+    Matrix* operator->() const;
+    Matrix& operator*() const;
+
+private:
+    class Storage {
+    public:
+        Storage(Matrix* mtx);
+        ~Storage();
+
+        void incr();
+        void decr();
+
+        int getCounter() const;
+        Matrix* getObject() const;
+
+    private:
+        Matrix *data_;
+        int ref_count_;
+    };
+
+private:
+    Storage *storage_;
+};
