efcd396ee041f1035651aebcbf62de8bf73f8932
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2019-12-06 22:34:49 +0300

    HSE/Lab 10: publish

---
+++ HSE_F19-S20/lab_10/include/Circle.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "figure.h"
+
+class Circle : public Figure {
+public:
+    Circle(int id, int x, int y, int radius, const char* label);
+
+    // ...
+};
---
+++ HSE_F19-S20/lab_10/include/Rectangle.hpp
@@ -0,0 +1,8 @@
+#include "figure.h"
+
+class Rectangle : public Figure {
+public:
+    Rectangle(int id, int x, int y, int width, int height);
+
+    // ...
+};
